62912303b84c4d385357a842f1b179dfde123feb
Calvin Lui <clui951@berkeley.edu>
2015-01-09 17:12:36 -0800

    Jsoup takes local file input to parse

--- src/HTML_SpellCheck.java
+++ src/HTML_SpellCheck.java
@@ -6,22 +6,13 @@ import org.jsoup.nodes.Document;
 
 public class HTML_SpellCheck {
 
-//	public static void main(String[] args) {
-//		// TODO Auto-generated method stub
-//		System.out.println("Running");
-//		WebPage page = new WebPage("file");
-//		page.readHTML();
-//	}
-
 	public static void main(String[] args) {
-		System.out.println("RUNNING MAIN \n");
-		try {
-			Document doc = Jsoup.connect("http://example.com/").get();
-			System.out.println("SUCCEED");
-		} catch (IOException e) {
-			System.out.println("\n\nFAIL");
-			e.printStackTrace();
-		}
+		// TODO Auto-generated method stub
+		System.out.println("Running");
+		WebPage page = new WebPage("../sample.html");
+		System.out.println(page.getHTML());
 	}
+
+	
 	
 }
--- src/WebPage.java
+++ src/WebPage.java
@@ -1,37 +1,54 @@
-//import java.io.IOException;
-//
-//import org.jsoup.Jsoup;
-//import org.jsoup.nodes.*;
-//
-//
-//public class WebPage {
-//
-//	private String fileName;
-//	private String html;
-//	
-//	// Constructor takes in file name or path to file 
-//	public WebPage( String fileName ) {
-//		this.fileName = fileName;
-//	}
-//	
-//	public void readHTML() {
-//		try {
-//			Document doc = Jsoup.connect("http://example.com/").get();
-//			System.out.println(doc.html());
-//		} catch (IOException e) {
-//			this.html = null;
-//			e.printStackTrace();
-//		}
-//		
-//	}
-//	
-//	
-//	
-//	
-//	/* Getters & Setters */
-//	
-//	public String getFileName() {
-//		return this.fileName;
-//	}
-//	
-//}
+import java.io.File;
+import java.io.IOException;
+
+// Jsoup dependencies
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.*;
+
+
+public class WebPage {
+
+	private String fileName;
+	private Document doc;
+	private Boolean valid;
+	
+	// Constructor takes in file name or path to file 
+	public WebPage( String fileName ) {
+		this.fileName = fileName;
+		this.readDoc();
+	}
+	
+	public void readDoc() {
+		try {
+//			this.doc = Jsoup.connect("http://example.com/").get();
+			this.doc = Jsoup.parse(new File(this.fileName), "UTF-8");
+			this.valid = true;
+		} catch (IOException e) {
+			this.doc = null;
+			this.valid = false;
+			e.printStackTrace();
+		}
+		
+	}
+	
+	
+	
+	
+	/* Getters & Setters */
+	
+	public String getFileName() {
+		return this.fileName;
+	}
+	
+	public String getHTML() {
+		if (this.valid) { 
+			return this.doc.html();
+		}
+		return null;
+	}
+	
+	public Boolean isValid() {
+		return this.valid;
+	}
+	
+}
